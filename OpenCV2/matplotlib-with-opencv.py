8ec3d637a3e43a8886ab4a77d6f817f548a08b16
shahriar <43968918+ShahSau@users.noreply.github.com>
2020-05-27 12:33:47 +0300

    Add files via upload

---
+++ OpenCV2/matplotlib-with-opencv.py
@@ -0,0 +1,18 @@
+import cv2
+
+from matplotlib import pyplot as plt
+
+
+img=cv2.imread("lena.jpg",-1)
+
+cv2.imshow("IMAGE", img)
+#CONVERT THE image from bgr to rgb
+img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+plt.imshow(img)
+#hide x and y ticks
+plt.xticks([]), plt.yticks([])
+
+
+plt.show()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
